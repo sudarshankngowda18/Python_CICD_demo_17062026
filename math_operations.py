3dd9e26ac956d29a9f26f6db9b251e77e2121efd
sudarshankngowda18 <sudarshankngowda@gmail.com>
2026-06-22 14:05:44 +0530

    corrected

--- math_operations.py
+++ math_operations.py
@@ -1,5 +1,7 @@
 def add(a, b):
+    print (a+b)
     return a + b
 
 def subtract(a, b):
+    print (a-b)
     return a - b
--- test_math_operations.py
+++ test_math_operations.py
@@ -1,8 +1,8 @@
 from math_operations import add, subtract
 
-def test_add(a,b):
+def test_add():
     assert add(3,2) == 5
 
-def test_subtract(a,b):
+def test_subtract():
     assert subtract(10, 4) == 6
 
